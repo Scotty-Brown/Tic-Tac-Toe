cb2b80b6ddb1536cf3e81d137c324f55b7071490
Scotty Brown <scottybrown98@gmail.com>
2023-06-12 12:29:44 -0700

    Add functionality to determine turn and place appropriate token on click box

--- main.js
+++ main.js
@@ -6,28 +6,30 @@ var nineBoxes = document.querySelectorAll('.box')
 
 var currentPlayers;
 var gameBoard = {
-    // players: [],
-    turn: 0,
+    gameBoardPositions: ['', '', '', '', '', '', '', '', ''],
+    turn: 2,
     // playerIcons: ['🧞‍♂️', '🧞‍♀️'],
     winningCombos: [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
-        [1,4,7],
-        [2,5,8],
-        [3,6,9],
-        [1,5,9],
-        [3,5,7],
+        ['1','2','3'],
+        ['4','5','6'],
+        ['7','8','9'],
+        ['1','4','7'],
+        ['2','5','8'],
+        ['3','6','9'],
+        ['1','5','9'],
+        ['3','5','7'],
     ]
 }
 // have choose their own icon
 
 // event listeners
-document.addEventListener('DOMContentLoaded', setGame)
+document.addEventListener('DOMContentLoaded', function() {
+    setGame()
+})
 
 nineBoxes.forEach(function(box) {
-    box.addEventListener('click', function() {
-        placeToken(gameBoard);
+    box.addEventListener('click', function(event) {
+    placeToken(event);
     });
 });
 
@@ -50,9 +52,21 @@ function addPlayersToGameboard() {
 
 function setGame() {
     gameBoard = addPlayersToGameboard()
-    gameBoard.players[0].isTurn = true
-    gameBoard.players[1].isTurn = false
+    var firstPlayer = determineFirstTurn(gameBoard)
+    if (firstPlayer === 'alpha') {
+        gameBoard.players[0].isTurn = true
+        gameBoard.players[1].isTurn = false
+    } else if (firstPlayer === 'omega') {
+        gameBoard.players[0].isTurn = false
+        gameBoard.players[1].isTurn = false
+    }
 }
+
+// function updatePlayerPositions(gameBoard) {
+//     nineBoxes.forEach(function(box) {
+//         gameBoard.positions = event.target
+//         });
+//     }
 
 function determineFirstTurn(gameBoard) {
     if (gameBoard.turn % 2 === 0) {
@@ -64,6 +78,14 @@ function determineFirstTurn(gameBoard) {
         gameBoard.players[1].isTurn = true
         return 'omega'
     }
+}
+
+function determineTurn(gameBoard) {
+ for (var i = 0; i < gameBoard.players.length; i++) {
+    if (gameBoard.players[i].isTurn === true) {
+        return gameBoard.players[i].id
+    }
+ }
 }
 
 // function determineNonFirstTurn(gameBoard) {
@@ -92,13 +114,40 @@ function increaseWins(player) {
 
 
 //-------------- DOM FUNCTIONS -----------------//
-function placeToken(gameBoard) {
-    for (var i = 0; i< gameBoard.players.length; i++) {
-        if (gameBoard.players[i].isTurn) {
-            event.target.innerHTML += `<span role="img" aria-label="red-icon" title="red-icon">${gameBoard.players[i].token}</span>`
-        }
-    } swapTurns()
-}
+  
+  function placeToken(event) {
+    var alphaToken = gameBoard.players[0].token;
+    var omegaToken = gameBoard.players[1].token;
+    console.log(event.target);
+  
+    var clickedIndex = Array.from(nineBoxes).indexOf(event.target);
+    
+    if (gameBoard.gameBoardPositions.every(position => position === '')) {
+      if (determineFirstTurn(gameBoard) === 'alpha') {
+        gameBoard.gameBoardPositions[clickedIndex] = alphaToken;
+        event.target.innerHTML += alphaToken;
+      } else if (determineFirstTurn(gameBoard) === 'omega') {
+        gameBoard.gameBoardPositions[clickedIndex] = omegaToken;
+        event.target.innerHTML += omegaToken;
+      }
+    } else {
+      if (determineTurn(gameBoard) === 'alpha') {
+        gameBoard.gameBoardPositions[clickedIndex] = alphaToken;
+        event.target.innerHTML += alphaToken;
+      } else if (determineTurn(gameBoard) === 'omega') {
+        gameBoard.gameBoardPositions[clickedIndex] = omegaToken;
+        event.target.innerHTML += omegaToken;
+      }
+    }
+    
+    swapTurns();
+  }
+  
+
+
+
+// event.target.innerHTML += `<span role="img" aria-label="red-icon" title="red-icon">${gameBoard.players[i].token}</span>`
+
 
 
 // function switchPlayers() {
